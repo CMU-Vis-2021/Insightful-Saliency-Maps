6c3dd79f8b4a19cc34a088730091216d3e9adc29
Swetha Kannan <swetha@MacBook-Air-4.socal.rr.com>
2021-10-30 01:15:57 -0400

    design work

--- main.js
+++ main.js
@@ -1,27 +1,65 @@
-import * as d3 from "d3";
-import vegaEmbed from "vega-embed";
+// import * as d3 from "d3";
+// import vegaEmbed from "vega-embed";
 
-d3.select("#d3-div").append("p").text("hello from D3");
+// d3.select("#d3-div").append("p").text("hello from D3");
 
-vegaEmbed("#vega-div", {
-  $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-  description: "A simple bar chart with embedded data.",
-  data: {
-    values: [
-      { a: "A", b: 28 },
-      { a: "B", b: 55 },
-      { a: "C", b: 43 },
-      { a: "D", b: 91 },
-      { a: "E", b: 81 },
-      { a: "F", b: 53 },
-      { a: "G", b: 19 },
-      { a: "H", b: 87 },
-      { a: "I", b: 52 },
-    ],
-  },
-  mark: "bar",
-  encoding: {
-    x: { field: "a", type: "nominal", axis: { labelAngle: 0 } },
-    y: { field: "b", type: "quantitative" },
-  },
-});
+// vegaEmbed("#vega-div", {
+//   $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+//   description: "A simple bar chart with embedded data.",
+//   data: {
+//     values: [
+//       { a: "A", b: 28 },
+//       { a: "B", b: 55 },
+//       { a: "C", b: 43 },
+//       { a: "D", b: 91 },
+//       { a: "E", b: 81 },
+//       { a: "F", b: 53 },
+//       { a: "G", b: 19 },
+//       { a: "H", b: 87 },
+//       { a: "I", b: 52 },
+//     ],
+//   },
+//   mark: "bar",
+//   encoding: {
+//     x: { field: "a", type: "nominal", axis: { labelAngle: 0 } },
+//     y: { field: "b", type: "quantitative" },
+//   },
+// });
+
+let tab1 = document.getElementById("tab1");
+let tab2 = document.getElementById("tab2");
+let tab3 = document.getElementById("tab3");
+
+let tab1Button = document.getElementById("openTab1");
+let tab2Button = document.getElementById("openTab2");
+let tab3Button = document.getElementById("openTab3");
+
+
+function changeTab(button, tabToReveal){
+    
+    if(button.classList.contains("active") != true){
+        tab1Button.classList = "";
+        tab2Button.classList = "";
+        tab3Button.classList = "";
+        button.classList = "active"
+
+        if(tabToReveal == "tab1")
+            {
+                tab1.classList= "";
+                tab2.classList= "hidden";
+                tab3.classList= "hidden";
+            }
+        else if(tabToReveal == "tab2")
+        {
+            tab1.classList= "hidden";
+            tab2.classList= "";
+            tab3.classList= "hidden";
+        }
+        else if(tabToReveal == "tab3")
+        {
+            tab1.classList= "hidden";
+            tab2.classList= "hidden";
+            tab3.classList= "";
+        }
+    }
+}
