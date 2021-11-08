456d372a3d3718db34943a6211c16ba90fe697d0
Swetha Kannan <swetha@MacBook-Air-4.local>
2021-11-08 17:59:20 -0500

    some canvas changes and quiz has an ending

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // import * as d3 from "d3";
 
+var colorRed = '#e32d2d';
+var colorOrange = '#e8900c';
+var colorPurple = '#793ac2';
+
+var allImages = [];
+var selectedAnswer = [];
+var rightAnswer = [];
+var numberOfQuestions = 4;
+
 let tab1 = document.getElementById("tab1");
 let tab2 = document.getElementById("tab2");
 let tab3 = document.getElementById("tab3");
@@ -39,8 +48,12 @@ window.onload = function() {
   var c = document.getElementById("canvas");
   var ctx = c.getContext("2d");
   var img = document.getElementById("canvasImg");
-  console.log(img)
-  ctx.drawImage(img, 0, 0);
+  img.onload = function() {
+    // At this point, the image is fully loaded
+    ctx.drawImage(img, 0, 0,c.width,c.height);
+};
+  console.log(img);
+  console.log(c.width);
 }
 
 
@@ -136,69 +149,132 @@ function changeImage(progress){
     
     console.log(shapeList)
     var select = document.getElementById('quizselect')
+    selectedAnswer.push(select.value);
     console.log(select)
     select.value = "-1";
     console.log(select)
 
-    // choose an image class and random image within that class
+    if(parseInt(progress.textContent) == 4){
+        console.log("show Finale");
+        showQuizFinal();
+    }
+    else{
 
-    var selection_shape = shapeList[Math.floor(Math.random()*shapeList.length)];
-    var selection_texture = textureList[Math.floor(Math.random()*textureList.length)];
-    console.log("current selection shape: ", selection_shape)
-    // var imgNum = Math.floor(Math.random()*10)+1;
-    var quizImage = document.getElementById("quizImg");
-    var classpath = "./assets/stylized-images/"+selection_shape+"-stylized-"+selection_texture+'.jpg';
-    
-    // var randidx = parseddata[selection][imgNum][Math.floor(Math.random()*parseddata[selection][imgNum].length)]
-    // var path = classpath + randidx;
-
-
-    var quizImage = $('#quizImg')
-
-    $.ajax({
-        url: classpath,
-        type: "GET"
-    }).done(function() {
-        quizImage.attr('src', classpath);   // set the image source
-    }).fail(function() {
-        quizImage.hide();    // or something other
-    });
-
-    predictClass(document.getElementById('quizImg'));
-
-    // Calculate progress for next dot -- change button to say submit if on the last question
-    var nextnum = parseInt(progress.textContent) + 1;
-    if (nextnum == 4){
-        var subbtn = document.getElementById('quizbtn')
-        subbtn.textContent = "Submit"
-    } 
-
-    // create current and next id based on progress
-    var nextid = "dot" + nextnum.toString() + " notactive";
-    var curid = "dot" + progress.textContent + " active";
-
-    // modify style of current dot
-    var currentdot = document.getElementById(curid);
-    currentdot.style.backgroundColor = "rgb(211, 211, 211)";
-    curid = "dot" + progress.textContent + " notactive";
-    currentdot.id = curid;
-
-    // modify style of next dot
-    var nextdot = document.getElementById(nextid);
-    nextdot.style.backgroundColor = "#3274b5";
-    nextdot.id = "dot" + nextnum.toString() + " active";
-
-    // update progress and image
-    progress.textContent = nextnum;
+        // choose an image class and random image within that class
+        var selection_shape = shapeList[Math.floor(Math.random()*shapeList.length)];
+        var selection_texture = textureList[Math.floor(Math.random()*textureList.length)];
+        console.log("current selection shape: ", selection_shape)
+        rightAnswer.push(selection_shape);
+        // var imgNum = Math.floor(Math.random()*10)+1;
+        var quizImage = document.getElementById("quizImg");
+        var classpath = "./assets/stylized-images/"+selection_shape+"-stylized-"+selection_texture+'.jpg';
+        
+        allImages.push(classpath);
+        // var randidx = parseddata[selection][imgNum][Math.floor(Math.random()*parseddata[selection][imgNum].length)]
+        // var path = classpath + randidx;
 
 
-    // remove that option from the image selection list
-    shapeList = shapeList.filter(function(value, index, arr){
-        return value != selection_shape;
-    });
-    textureList = textureList.filter(function(value, index, arr){
-        return value != selection_texture;
-    });
+        var quizImage = $('#quizImg')
+
+        $.ajax({
+            url: classpath,
+            type: "GET"
+        }).done(function() {
+            quizImage.attr('src', classpath);   // set the image source
+        }).fail(function() {
+            quizImage.hide();    // or something other
+        });
+
+        predictClass(document.getElementById('quizImg'));
+
+        // Calculate progress for next dot -- change button to say submit if on the last question
+        var nextnum = parseInt(progress.textContent) + 1;
+        if (nextnum == 4){
+            var subbtn = document.getElementById('quizbtn')
+            subbtn.textContent = "Submit"
+        } 
+
+
+        // create current and next id based on progress
+        var nextid = "dot" + nextnum.toString() + " notactive";
+        var curid = "dot" + progress.textContent + " active";
+
+        // modify style of current dot
+        var currentdot = document.getElementById(curid);
+        currentdot.style.backgroundColor = "rgb(211, 211, 211)";
+        curid = "dot" + progress.textContent + " notactive";
+        currentdot.id = curid;
+
+        // modify style of next dot
+        var nextdot = document.getElementById(nextid);
+        nextdot.style.backgroundColor = "#3274b5";
+        nextdot.id = "dot" + nextnum.toString() + " active";
+
+        // update progress and image
+        progress.textContent = nextnum;
+
+
+        // remove that option from the image selection list
+        shapeList = shapeList.filter(function(value, index, arr){
+            return value != selection_shape;
+        });
+        textureList = textureList.filter(function(value, index, arr){
+            return value != selection_texture;
+        });
+    }
+}
+
+function showQuizFinal(){
+    let quizDiv = document.getElementById('quiz-before');
+    let answerDiv = document.getElementById('quiz-after');
+    let answersRight = 0;
+
+    quizDiv.style.display = "none";
+    answerDiv.style.display = "block";
+    // console.log("allImages = ");
+    // console.log(allImages);
+    // console.log("selectedAnswer = ");
+    // console.log(selectedAnswer);
+    // console.log("rightAnswer = ");
+    // console.log(rightAnswer);
+
+    let templateAnswer = document.getElementById('answer-template');
+    for(let i = 0; i < numberOfQuestions; i++){
+        const answerElement = templateAnswer.cloneNode(true);
+        answerElement.id = "";
+
+        const imageElement = answerElement.getElementsByClassName("image")[0];
+        imageElement.src = allImages[i];
+        
+        const userElement = answerElement.getElementsByClassName("yourAnswer")[0];
+        if(selectedAnswer[i] == "-1"){
+            selectedAnswer[i] = "nothing selected"
+        }
+        userElement.innerHTML = selectedAnswer[i];
+        if(selectedAnswer[i] == rightAnswer[i])
+            userElement.style.color = "green";
+        else
+            userElement.style.color = "red";
+
+        const predictionElement = answerElement.getElementsByClassName("AIprediction")[0];
+        predictionElement.innerHTML = rightAnswer[i];
+        if(rightAnswer[i] == rightAnswer[i])
+            predictionElement.style.color = "green";
+        else
+            predictionElement.style.color = "red";
+
+        const rightAnswerElement = answerElement.getElementsByClassName("correctAnswer")[0];
+        rightAnswerElement.innerHTML = rightAnswer[i];
+        rightAnswerElement.style.color = "green";
+
+        if(selectedAnswer[i] == rightAnswer[i]){
+            answersRight += 1;
+        }
+        answerDiv.append(answerElement);
+    }
+    let answersRightElement = answerDiv.getElementsByClassName('number')[0];
+    answersRightElement.innerHTML = answersRight;
+
 }
 
 function predictClass(image){
@@ -222,10 +298,11 @@ function firstImage(){
     var selection_shape = shapeList[Math.floor(Math.random()*shapeList.length)];
     var selection_texture = textureList[Math.floor(Math.random()*textureList.length)];
     console.log("current selection shape: ", selection_shape)
+    rightAnswer.push(selection_shape);
     // var imgNum = Math.floor(Math.random()*10)+1;
     var quizImage = document.getElementById("quizImg");
     var classpath = "./assets/stylized-images/"+selection_shape+"-stylized-"+selection_texture+'.jpg';
-    
+    allImages.push(classpath);
     // var randidx = parseddata[selection][imgNum][Math.floor(Math.random()*parseddata[selection][imgNum].length)]
     // var path = classpath + randidx;
 
