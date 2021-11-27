c7e1aaa02a8699365a78ff316b9e292b4898fbad
Katie Morrison <katelyncmorrison98@gmail.com>
2021-11-27 17:40:07 -0600

    overlap and images

--- js/stylize.js
+++ js/stylize.js
@@ -1,39 +1,9 @@
 function shapeImg(){
     var selectShape = document.getElementById('shapeselect'+count.toString())
-    console.log(selectShape)
-    console.log(selectShape.value)
-
-    var quizImage = $('#shapeimg'+count.toString())
-
-    classpath = "./assets/shapes/"+selectShape.value+".jpg";
-
-    $.ajax({
-        url: classpath,
-        type: "GET"
-    }).done(function() {
-        quizImage.attr('src', classpath);   // set the image source
-    }).fail(function() {
-        quizImage.hide();    // or something other
-    });
 }
 
 function textureImg(){
     var selectTexture = document.getElementById('textureselect'+count.toString())
-    console.log(selectTexture)
-    console.log(selectTexture.value)
-
-    var quizImage = $('#textureimg'+count.toString())
-
-    classpath = "./assets/textures/"+selectTexture.value+".jpg";
-
-    $.ajax({
-        url: classpath,
-        type: "GET"
-    }).done(function() {
-        quizImage.attr('src', classpath);   // set the image source
-    }).fail(function() {
-        quizImage.hide();    // or something other
-    });
 }
 
 function input3enable(){
@@ -53,7 +23,7 @@ function stylizeImg(){
 
     classpath = "./assets/stylized-images/"+selectShape.value+"-stylized-"+ selectTexture.value + ".jpg";
     updateBarPrediction(selectShape.value+"-stylized-"+ selectTexture.value);
-    console.log("FILE NAME ===   "+selectShape.value+"-stylized-"+ selectTexture.value + ".jpg")
+
     $.ajax({
         url: classpath,
         type: "GET"
@@ -64,7 +34,7 @@ function stylizeImg(){
     });
 
     if(document.getElementById('tab1xrai').value == "checked"){
-        xraioverlap(choice = document.getElementById("shapeselect0"), id = 'stylizedimg', query = '.flex #stylized-img-div1', appendHTML = '<img id="stylizedimg0" style="position: relative; margin: 0px; max-width: 230px; width:100%;"> <img id="xrai-img1" style="position: absolute; opacity: 40%; max-width: 230px;width:100%;"> ', sliders = document.getElementById("sliderTab1"));
+        xraioverlap(choice = document.getElementById("shapeselect0"), id = 'stylizedimg', query = '.flex #stylized-img-div1', appendHTML = '<img id="stylizedimg0" style="position: relative; margin: 0px; max-width: 230px; width:100%;"> <img id="xrai-img1" style="position: absolute; opacity: ' + document.querySelector("#sliderOpacity1").value + '%; max-width: 230px;width:100%;"> ', sliders = document.getElementById("sliderTab1"));
     }  
     
 
